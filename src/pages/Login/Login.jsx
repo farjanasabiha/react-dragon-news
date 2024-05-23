ade9241c5c58197a702faf4d907c362894e20bc2
devsabiha <farjanasabiha786@gmail.com>
2024-05-23 15:56:32 +0600

    first commit

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,10 @@
+import Navbar from "../Shared/Navbar/Navbar";
+const Login = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>This is login</h1>
+    </div>
+  );
+};
+export default Login;
